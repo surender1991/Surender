46cec600ed99069f87fe1c0f4d54eaf31f1fcdea
surender1991 <76657265+surender1991@users.noreply.github.com>
2020-12-27 23:13:02 +0530

    Add files via upload

---
+++ Selecto/sur/Magento_Theme/requirejs-config.js
@@ -0,0 +1,15 @@
+/**
+ * Copyright © Magento, Inc. All rights reserved.
+ * See COPYING.txt for license details.
+ */
+
+var config = {
+    paths: {
+        slick:'js/slick'
+    },
+    shim: {
+        slick: {
+            deps: ['jquery']
+        }
+    }
+};
